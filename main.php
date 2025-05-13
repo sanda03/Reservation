95015b5d08b76cebdfd52c39eca0d939818e2772
sanda03 <128069441+sanda03@users.noreply.github.com>
2025-05-13 19:21:10 +0300

    chore: add example

--- main.php
+++ main.php
@@ -8,18 +8,14 @@ $reservationService = new ReservationService();
 $car = new Item("Voiture");
 $house = new Item("Maison");
 
-// Réservation d'une après-midi (même jour)
 $start = DateUtils::now();
 $endSameDay = DateUtils::plusHours(5);
-
 echo "\n🔵 Réservation d'une après-midi (même jour) :\n";
 $reservationService->reserveItem($car, $start, $endSameDay);
 
-// Réservation sur plusieurs jours
 $endNextDay = DateUtils::plusDays(1);
 echo "\n🔵 Réservation sur deux jours :\n";
 $reservationService->reserveItem($house, $start, $endNextDay);
 
-// Liste des réservations
 $reservationService->listReservations();
 ?>
